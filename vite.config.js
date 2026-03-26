882e909437f4a1f20e51fdabbe0f74434b588d93
zencara-s15 <chinsereyputhireach@gmail.com>
2026-03-26 15:40:38 +0700

    [Config] allow all hosting

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
-    preview: {
-      host: '0.0.0.0',
-      port: env.PORT || 4173,
-      allowedHosts: env.ALLOWED_HOSTS ? env.ALLOWED_HOSTS.split(',') : [],
-    },
-  }
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: 'all',
+  },
 })
