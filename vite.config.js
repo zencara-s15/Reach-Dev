4e29baa1f9cddcc5ab481f25d88486030cbd1fb1
zencara-s15 <chinsereyputhireach@gmail.com>
2026-03-26 15:50:59 +0700

    [Config] allow all hosting

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 4173,
-    allowedHosts: 'all',
+    allowedHosts: 'reach-demo.onrender.com',
   },
 })
